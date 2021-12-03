55b817c3e552ac04a6a6e051dbf94d1e0c3f6718
spomaraiko <spomaraiko@exadel.com>
2021-12-03 09:50:56 +0200

    Perspective decoration (excercise 4)

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,7 +30,8 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
-      <MaxWidthWrapper className={styles.maxWidthWrapper}>
+      <div className={styles.decorationBar}/>
+      <MaxWidthWrapper className={`${styles.maxWidthWrapper} ${styles.isolation}`}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
@@ -77,6 +78,8 @@ function App() {
           />
         </div>
       </MaxWidthWrapper>
+
+
 
       <div className={styles.characterWrapper}>
         <Character
